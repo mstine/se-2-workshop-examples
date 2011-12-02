a73d4dd83eabcee6ba13a14927dff3a5fab21928
Matt Stine <matt.stine@gmail.com>
2011-12-01 23:02:03 -0500

    create hello world test to vet environment

---
+++ studentSandbox/src/test/java/com/dss/se2workshop/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package com.dss.se2workshop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void itWorks() {
+        Assert.assertTrue(true);
+    }
+}
