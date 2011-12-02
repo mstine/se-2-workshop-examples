47b0215ad9a3d0090d0adae386aa0d4c8bfae778
Matt Stine <matt.stine@gmail.com>
2011-12-01 23:18:01 -0500

    set up property loader for sandbox

--- studentSandbox/src/test/java/com/dss/se2workshop/HelloWorldTest.java
+++ studentSandbox/src/test/java/com/dss/se2workshop/HelloWorldTest.java
@@ -1,5 +1,6 @@
 package com.dss.se2workshop;
 
+import com.dss.se2workshop.util.PropertyLoader;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,5 +9,7 @@ public class HelloWorldTest {
     @Test
     public void itWorks() {
         Assert.assertTrue(true);
+
+        Assert.assertEquals("http://content1.local:8080", PropertyLoader.getBaseUrl());
     }
 }
